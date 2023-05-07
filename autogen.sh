7a82f5e501211b960f9009eb49bbbbca62ede05d
Guenther Brunthaler <gb@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2023-05-07 03:00:35 +0200

    Remove unnecessary source definition

--- autogen.sh
+++ autogen.sh
@@ -4,5 +4,11 @@
 #
 # v2023.127
 
-touch NEWS README AUTHORS ChangeLog
+set -e
+trap 'test $? = 0 || echo "\"$0\" failed!" >& 2' 0
+for f in NEWS README AUTHORS ChangeLog
+do
+	test -e $f && continue
+	> "$f"
+done
 autoreconf -i
