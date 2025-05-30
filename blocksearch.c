e39c404973726944278774890f4492738f1aca47
Guenther Brunthaler <gb@xrpi4.emgenxx69lwyn5ctlr4nl64ul.local>
2025-05-30 14:24:42 +0200

    Add README and reformat assignments
    Use the following command for reformatting:
    
    $ perl -i -pe 's!(\w|[])}])((?:[-+/*&|^]|[<>]{2})?=\s)!\1 \2!g' \
      blocksearch.c

--- blocksearch.c
+++ blocksearch.c
@@ -1,12 +1,12 @@
-static char VERSION_INFO[]= {
-   "Version 2023.126\n"
-   "Copyright (c) 2022-2023 Guenther Brunthaler. All rights reserved.\n"
+static char VERSION_INFO[] = {
+   "Version 2025.150\n"
+   "Copyright (c) 2022-2025 Guenther Brunthaler. All rights reserved.\n"
    "\n"
    "This program is free software.\n"
    "Distribution is permitted under the terms of the GPLv3.\n"
 };
 
-static char HELP[]= {
+static char HELP[] = {
    "\n"
    "Usage: %s [ <options> ... [--] ]\n"
    "       <buffer_size> <haystack> [ <start> ] < <needle>\n"
@@ -56,7 +56,7 @@ static void release_tracked_until(resource *stop) {
 }
 
 static void die(char const *emsg) {
-   if (!errors) errors= -1; /* Means 1 or more errors. */
+   if (!errors) errors = -1; /* Means 1 or more errors. */
    (void)fprintf(stderr, "%s\n", emsg);
    release_tracked_until(0);
    exit(EXIT_FAILURE);
@@ -64,20 +64,20 @@ static void die(char const *emsg) {
 
 static void *malloc_ck(size_t bytes) {
    void *memblk;
-   if (memblk= malloc(bytes)) return memblk;
+   if ((memblk = malloc(bytes)) != 0) return memblk;
    die("Out of memory!");
 }
 
 static void track_resource(resource *new_rsc, void (*new_action)()) {
-   new_rsc->action= new_action;
-   new_rsc->older= rlist;
-   rlist= new_rsc;
+   new_rsc->action = new_action;
+   new_rsc->older = rlist;
+   rlist = new_rsc;
 }
 
 static void *untrack_resource(void) {
    resource *r;
    assert(rlist);
-   rlist= (r= rlist)->older;
+   rlist = (r = rlist)->older;
    return r;
 }
 
@@ -88,7 +88,7 @@ static void malloc_dtor(void) {
 static void *new_tracked_resource(size_t total_bytes) {
    resource *r;
    assert(total_bytes >= sizeof(*r));
-   track_resource(r= malloc_ck(total_bytes), &malloc_dtor);
+   track_resource(r = malloc_ck(total_bytes), &malloc_dtor);
    return r;
 }
 
@@ -104,7 +104,7 @@ typedef struct {
 } buffer_resource;
 
 static void buffer_dtor(void) {
-   buffer_resource *br= untrack_resource();
+   buffer_resource *br = untrack_resource();
    if (br->descriptor.capacity) {
       assert(br->descriptor.length <= br->descriptor.capacity);
       assert(br->descriptor.start);
@@ -116,10 +116,10 @@ static void buffer_dtor(void) {
 }
 
 static slice *new_tracked_buffer(void) {
-   buffer_resource *br= new_tracked_resource(sizeof *br);
-   br->descriptor.start= 0;
-   br->descriptor.capacity= br->descriptor.length= 0;
-   br->rsrc.action= buffer_dtor;
+   buffer_resource *br = new_tracked_resource(sizeof *br);
+   br->descriptor.start = 0;
+   br->descriptor.capacity = br->descriptor.length = 0;
+   br->rsrc.action = buffer_dtor;
    return &br->descriptor;
 }
 
@@ -131,7 +131,7 @@ static void *realloc_ck(void *p, size_t newsz) {
       return 0;
    }
    if (!p) return malloc_ck(newsz);
-   if (pnew= realloc(p, newsz)) return pnew;
+   if ((pnew = realloc(p, newsz)) != 0) return pnew;
    die("Could not resize the memory allocation!");
 }
 
@@ -139,9 +139,9 @@ static void grow_buffer(slice *buf, size_t bigger_minimum_size) {
    size_t bytes;
    assert(bigger_minimum_size >= buf->length);
    assert(bigger_minimum_size > buf->capacity);
-   for (bytes= 1; bytes < bigger_minimum_size; bytes+= bytes) {}
-   buf->start= realloc_ck(buf->start, bytes);
-   buf->capacity= bytes;
+   for (bytes = 1; bytes < bigger_minimum_size; bytes += bytes) {}
+   buf->start = realloc_ck(buf->start, bytes);
+   buf->capacity = bytes;
 }
 
 static void grow_buffer_by(slice *buf, size_t increment) {
@@ -151,14 +151,14 @@ static void grow_buffer_by(slice *buf, size_t increment) {
 
 static void resize_buffer(slice *buf, size_t exact_new_size) {
    assert(exact_new_size >= buf->length);
-   buf->start= realloc_ck(buf->start, exact_new_size);
-   buf->capacity= exact_new_size;
+   buf->start = realloc_ck(buf->start, exact_new_size);
+   buf->capacity = exact_new_size;
 }
 
 static size_t fread_ck(void *dst, size_t elem_size, size_t n_elem, FILE *fh) {
    size_t read;
    assert(dst); assert(elem_size); assert(n_elem); assert(fh);
-   if ((read= fread(dst, elem_size, n_elem, fh)) != n_elem) {
+   if ((read = fread(dst, elem_size, n_elem, fh)) != n_elem) {
       if (ferror(fh)) die("Read error!");
       assert(feof(fh));
    }
@@ -167,14 +167,14 @@ static size_t fread_ck(void *dst, size_t elem_size, size_t n_elem, FILE *fh) {
 
 static slice *tracked_stream_remainder(FILE *fh) {
    slice *buf;
-   grow_buffer(buf= new_tracked_buffer(), 128);
+   grow_buffer(buf = new_tracked_buffer(), 128);
    assert(!buf->length);
    for (;;) {
       size_t read;
       if (buf->length == buf->capacity) grow_buffer_by(buf, 1);
       if (
          !(
-            read= fread_ck(
+            read = fread_ck(
                   buf->start + buf->length
                ,  sizeof(char)
                ,  buf->capacity - buf->length
@@ -184,7 +184,7 @@ static slice *tracked_stream_remainder(FILE *fh) {
       ) {
          break;
       }
-      buf->length+= read;
+      buf->length += read;
    }
    resize_buffer(buf, buf->length);
    return buf;
@@ -200,7 +200,7 @@ static void fflush_ck(FILE *stream) {
 
 static void freopen_ck(const char *pathname, const char *mode, FILE *stream) {
    FILE *result;
-   if ((result= freopen(pathname, mode, stream)) != stream) {
+   if ((result = freopen(pathname, mode, stream)) != stream) {
       assert(!result);
       die("Could not open stream!");
    }
@@ -210,39 +210,39 @@ static void putchar_ck(int chr) {
    if (putchar(chr) != chr) write_error();
 }
 
-static char xdigits[]= "0123456789abcdef";
+static char xdigits[] = "0123456789abcdef";
 
 static void print_size_t(size_t value) {
    if (value >= 0x10) {
       print_size_t(value >> 4);
-      value&= 0xf;
+      value &= 0xf;
    }
    putchar_ck(xdigits[value]);
 }
 
 static off_t convert_off_t(char const *hex) {
-   static char map[]= "AaBbCcDdEeFf";
-   off_t value= 0;
+   static char map[] = "AaBbCcDdEeFf";
+   off_t value = 0;
    if (!*hex) die("Number without any digits!");
    do {
       off_t nvalue;
       char *found;
       {
          int digit;
-         if (found= strchr(map, (digit= *hex))) {
+         if ((found = strchr(map, (digit = *hex))) != 0) {
             int off;
-            if ((off= (int)(found - map) & 1) == 0) {
-               digit= map[off + 1];
+            if ((off = (int)(found - map) & 1) == 0) {
+               digit = map[off + 1];
             }
          }
-         if (!(found= strchr(xdigits, digit))) {
+         if (!(found = strchr(xdigits, digit))) {
             die("Invalid hexadecimal digit in number!");
          }
       }
-      if ((nvalue= value << 4 | (off_t)(found - xdigits)) < value) {
+      if ((nvalue = value << 4 | (off_t)(found - xdigits)) < value) {
          die("Hexadecimal number exceeds its supported maximum value!");
       }
-      value= nvalue;
+      value = nvalue;
    } while (*++hex);
    return value;
 }
@@ -256,12 +256,12 @@ static int scan4match(
    {
       size_t read;
       while (
-         read= fread_ck(
+         read = fread_ck(
             work->start, sizeof(char), work->capacity, haystack
          )
       ) {
          size_t already_matched, boff;
-         already_matched= boff= 0;
+         already_matched = boff = 0;
          while (boff != read) {
             if (needle->start[already_matched] == work->start[boff]) {
                if (++already_matched == needle->length) {
@@ -269,12 +269,12 @@ static int scan4match(
                   goto done;
                }
             } else if (already_matched) {
-               boff-= already_matched;
-               already_matched= 0;
+               boff -= already_matched;
+               already_matched = 0;
             }
             ++boff;
          }
-         fpos+= read;
+         fpos += read;
       }
    }
    done:
@@ -283,8 +283,8 @@ static int scan4match(
 
 static off_t lseek_ck(int fildes, off_t offset, int whence) {
    off_t new;
-   errno= 0;
-   if ((new= lseek(fildes, offset, whence)) != -1 || !errno) return new;
+   errno = 0;
+   if ((new = lseek(fildes, offset, whence)) != -1 || !errno) return new;
    die("Failure changing the current file offset position!");
 }
 
@@ -294,7 +294,7 @@ typedef struct {
 } usage_resource;
 
 static void usage_action(void) {
-   usage_resource *r= untrack_resource();
+   usage_resource *r = untrack_resource();
    if (errors) (void)fprintf(stderr, HELP, r->prog_name, VERSION_INFO);
 }
 
@@ -307,29 +307,29 @@ int main(int argc, char **argv) {
    usage_resource usage;
    resource flusher;
    slice *needle, *work;
-   off_t start= 0;
+   off_t start = 0;
    track_resource(&flusher, &flusher_action);
    {
-      int arg= 0;
-      usage.prog_name= "(unnamed_program)";
+      int arg = 0;
+      usage.prog_name = "(unnamed_program)";
       track_resource(&usage.rsrc, &usage_action);
       if (arg == argc) {
          need_more:
          die("Too few arguments!");
       }
-      usage.prog_name= argv[arg];
+      usage.prog_name = argv[arg];
       if (++arg == argc) goto need_more;
       if (!strcmp(argv[arg], "--")) {
          if (++arg == argc) goto need_more;
       } else if (*argv[arg] == '-') {
          die("Unknown option!");
       }
-      resize_buffer(work= new_tracked_buffer(), convert_off_t(argv[arg]));
+      resize_buffer(work = new_tracked_buffer(), convert_off_t(argv[arg]));
       if (++arg == argc) goto need_more;
-      needle= tracked_stream_remainder(stdin);
+      needle = tracked_stream_remainder(stdin);
       freopen_ck(argv[arg], "r", stdin);
       if (++arg == argc) goto args_done;
-      lseek_ck(fileno(stdin), start= convert_off_t(argv[arg]), SEEK_SET);
+      lseek_ck(fileno(stdin), start = convert_off_t(argv[arg]), SEEK_SET);
       if (++arg == argc) goto args_done;
       die("Too many arguments!");
    }
